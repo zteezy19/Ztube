3cb887011a5f70033fb760aa3fd66c77871179ed
zteezy19 <waiyanzawtin01@protonmail.com>
2024-03-16 16:16:27 +0800

    Added Firebase dependencies and Upload & Get Videos Function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -11,12 +11,27 @@ import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
+const rawVideoBucketName = "ztz-yt-raw-videos";
+const videoCollectionId = "videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -27,4 +42,41 @@ export const createUser = functions.auth.user().onCreate((user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall(
+  {maxInstances: 1},
+  async (request) => {
+    // Check if the user is authenticated
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated."
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique filename for upload
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {url, fileName};
+  }
+);
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot = await firestore
+    .collection(videoCollectionId)
+    .limit(10)
+    .get();
+  return snapshot.docs.map((doc) => doc.data());
 });
